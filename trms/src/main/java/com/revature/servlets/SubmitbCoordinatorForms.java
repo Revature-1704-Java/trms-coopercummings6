956bf5c49430a297a9a4119df72a0e17aac1edc0
Cooper Cummings <coopercummings6@gmail.com>
2018-01-05 16:48:54 -0500

    fixed some bugs

--- trms/src/main/java/com/revature/servlets/SubmitbCoordinatorForms.java
+++ trms/src/main/java/com/revature/servlets/SubmitbCoordinatorForms.java
@@ -25,26 +25,30 @@ public class SubmitbCoordinatorForms extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-			if(session.isNew())
-			{
-				session.invalidate();				//new sessions shouldn't be here, send them to log in
-				response.sendRedirect("index.html");
-			}
-			else
-			{
-				Employee employee = (Employee)session.getAttribute("employee");	//get employee from session
-				RequestHandler rHandler = RequestHandler.getRequestHandler();
-				List<RequestForm> formsToUpdate = rHandler.getFormsForBenefitsCoordinatorApproval(employee.getId());	//get the employee's forms that need to be completed from database (update method requires a RequestForm Object)
-				for(RequestForm form : formsToUpdate)											//loop through the request forms
+			try {
+				if(session.isNew())
 				{
-					String approved = request.getParameter("form" + form.getRequestID() + "approval");//get the grade for that request
-					form.setbCoordinatorApproval(approved.equalsIgnoreCase("true"));			//if approved was selected
-					try {
-						rHandler.updateRequest(form);												//update request
-					} catch (NullPointerException e) {
-						// method throws nullpointerexception, but also successfully submits data to database, so just catching the exception and doing nothing.
+					session.invalidate();				//new sessions shouldn't be here, send them to log in
+					response.sendRedirect("index.html");
+				}
+				else
+				{
+					Employee employee = (Employee)session.getAttribute("employee");	//get employee from session
+					RequestHandler rHandler = RequestHandler.getRequestHandler();
+					List<RequestForm> formsToUpdate = rHandler.getFormsForBenefitsCoordinatorApproval(employee.getId());	//get the employee's forms that need to be completed from database (update method requires a RequestForm Object)
+					for(RequestForm form : formsToUpdate)											//loop through the request forms
+					{
+						String approved = request.getParameter("form" + form.getRequestID() + "approval");//get the grade for that request
+						form.setbCoordinatorApproval(approved.equalsIgnoreCase("true"));			//if approved was selected
+						try {
+							rHandler.updateRequest(form);												//update request
+						} catch (NullPointerException e) {
+							// method throws nullpointerexception, but also successfully submits data to database, so just catching the exception and doing nothing.
+						}
 					}
 				}
+			} catch (NullPointerException e) {
+				// method throws nullpointerexception, but also successfully submits data to database, so just catching the exception and doing nothing.
 			}
 
 		response.sendRedirect("http://localhost:8080/trms/ReimbursementManager");		//send back to reimbursement manager
